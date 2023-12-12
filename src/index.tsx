0bcfce0eaf887c9b5669954f7e43a28e3464b062
YarovoyDM <yarovoy.dmytro@gmail.com>
2023-12-12 04:05:52 +0200

    fix eslint

--- src/index.tsx
+++ src/index.tsx
@@ -8,15 +8,16 @@ import { Provider } from 'react-redux';
 import { store } from "./reducers";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </BrowserRouter>,
   </React.StrictMode>
 );
 
